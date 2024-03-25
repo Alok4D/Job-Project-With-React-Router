285ced330254527ced3247c98e61a9e78169dd76
Alok4D <alokroy602701@gmail.com>
2024-03-25 13:33:57 +0600

    add new card in my website

--- src/Components/Applied Jobs/AppliedJobs.jsx
+++ src/Components/Applied Jobs/AppliedJobs.jsx
@@ -8,20 +8,22 @@ const AppliedJobs = () => {
   const [appliedJobs, setAppliedJobs] = useState([]);
   const [displayJobs, setDisplayJobs] = useState([]);
 
-  const handleJobsFilter = filter => {
-    if(filter === 'all'){
-        setDisplayJobs(appliedJobs);
+  const handleJobsFilter = (filter) => {
+    if (filter === "all") {
+      setDisplayJobs(appliedJobs);
+    } else if (filter === "remote") {
+      const remoteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Remote"
+      );
+      setDisplayJobs(remoteJobs);
+    } else if (filter === "onsite") {
+      const onsiteJobs = appliedJobs.filter(
+        (job) => job.remote_or_onsite === "Onsite"
+      );
+      setDisplayJobs(onsiteJobs);
     }
-    else if(filter === 'remote'){
-        const remoteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Remote');
-        setDisplayJobs(remoteJobs);
-    }
-    else if(filter === 'onsite'){
-        const onsiteJobs = appliedJobs.filter(job => job.remote_or_onsite === 'Onsite');
-        setDisplayJobs(onsiteJobs);
-    }
-  }
-
+  };
+  console.log(appliedJobs);
   useEffect(() => {
     const storedJobId = getStoredJobApplication();
 
@@ -42,22 +44,78 @@ const AppliedJobs = () => {
   }, [jobs]);
   return (
     <div>
-      <h2 className="text-2xl">Jobs i applied! : {appliedJobs.length}</h2>
-
-      <details className="dropdown">
-        <summary className="m-1 btn">open or close</summary>
-        <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
-          <li onClick={() => handleJobsFilter('all')}><a>All</a></li>
-          <li onClick={() => handleJobsFilter('remote')}><a>Remote</a></li>
-          <li onClick={() => handleJobsFilter('onsite')}><a>OnSide</a></li>
-        </ul>
-      </details>
+      <div className="flex justify-between">
+        <h2 className="text-2xl">Jobs i applied : {appliedJobs.length}</h2>
+        <details className="dropdown">
+          <summary className="m-1 btn">open or close</summary>
+          <ul className="p-2 shadow menu dropdown-content z-[1] bg-base-100 rounded-box w-52">
+            <li onClick={() => handleJobsFilter("all")}>
+              <a>All</a>
+            </li>
+            <li onClick={() => handleJobsFilter("remote")}>
+              <a>Remote</a>
+            </li>
+            <li onClick={() => handleJobsFilter("onsite")}>
+              <a>OnSide</a>
+            </li>
+          </ul>
+        </details>
+      </div>
 
       <ul>
         {displayJobs.map((job) => (
           <li key={job.id}>
             <span>
-              {job.job_title} {job.company_name}: {job.remote_or_onsite}
+              <div className="flex justify-between w-[1920] mx-auto">
+                <div className="view-details-card">
+                  <div className="flex gap-6 m-3">
+                    <div className="h-[240px] w-[240px] bg-[#F4F4F4] flex justify-center items-center">
+                      <img
+                        src={job.logo}
+                        alt=""
+                        className="w-[145px] h-[50px]"
+                      />
+                    </div>
+
+                    <div>
+                      <h2>Technical Database Engineer</h2>
+                      <span>{job.company_name}</span>
+
+                      <div>
+                        <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE] mr-4">
+                          {job.remote_or_onsite}
+                        </button>
+                        <button className="px-5 py-2 font-extrabold border rounded border-[#7E90FE] text-[#7E90FE] mr-4">
+                          {job.job_type}
+                        </button>
+                      </div>
+
+                      <div className="flex gap-3">
+                        <div className="flex gap-2">
+                          <img
+                            src="../../../public/assets/icons/Location.png"
+                            alt=""
+                          />
+                          <span>{job.location}</span>
+                        </div>
+
+                        <div className="flex gap-2">
+                          <img
+                            src="../../../public/assets/icons/money.png"
+                            alt=""
+                          />
+                          <span>{job.salary}</span>
+                        </div>
+                      </div>
+
+                    </div>
+                  </div>
+                </div>
+
+                <div className="view-details">
+                  <button className="btn btn-primary">View Details</button>
+                </div>
+              </div>
             </span>
           </li>
         ))}
--- src/Components/Job Details/JobDetails.jsx
+++ src/Components/Job Details/JobDetails.jsx
@@ -20,13 +20,25 @@ const  JobDetails = () => {
 
     return (
         <div>
-            <h2>Job Details of : {job.job_title}</h2>
-            <div className="md:grid grid-cols-4 gap-4">
-            <div className="border md:col-span-3">
-                <h2>Details Coming Here!</h2>
+            {/* <h2>Job Details of : {job.job_title}</h2> */}
+            <div className="flex gap-6">
+
+            <div className="border md:col-span-3 w-[760px]">
+                <h2 className="text-[#1A1919]">Job Description:<p>A UI/UX (User Interface/User Experience) designer is responsible for designing and creating engaging and effective interfaces for software and web applications. This includes designing the layout, visual design, and interactivity of the user interface.</p></h2>
+                <p>Job Responsibility: Collaborating with cross-functional teams: UI/UX designers often work closely with other teams, including product management, engineering, and marketing, to ensure that the user interface is aligned with business and technical requirements. You will need to be able to effectively communicate your design ideas and gather feedback from other team members.</p>
+                <h2>Educational Requirements:</h2>
+                <p>Bachelor degree to complete any reputational university.</p>
+                <h3>Experiences:</h3>
+                <p>2-3 Years in this field.</p>
             </div>
+
             <div className="border">
-                <h2>side Things</h2>
+                <h2>Job Details</h2>
+                <hr />
+                <div className="salary flex gap-2">
+                    <img src="../../../public/assets/icons/money.png" alt="" />
+                    <span>Salary : <p> 100K - 150K (Per Month)</p></span>
+                </div>
                 <button onClick={handleApplyJobs} className="btn btn-primary w-full">Apply Now</button>
             </div>
             </div>
